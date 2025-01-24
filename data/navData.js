da9e38e08d0e3326ebcedba5d839c4f1e8ba919e
geonKO <kws07042@gmail.com>
2025-01-24 22:49:21 +0900

    feat: 네비게이션 데이터 및 컴포넌트 생성 및 디자인 -dom.js 내부에 nav,nav_list 변수 추가 -navigation.js 컴포넌트 생성 -navData.js 데이터파일 생성 -index.html에 navigation 컴포넌트 모듈 스크립트 추가

---
+++ data/navData.js
@@ -0,0 +1,15 @@
+export const NAV_DATA = [
+    {
+        name: "회사소개",
+        url: "/pages/feature/index.html",
+    },
+    {
+        name: "비전",
+        url: "/vision",
+    },
+    {
+        name: "친환경 무 폐수 날염",
+        url: "/eco",
+    },
+];
+
